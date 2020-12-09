d1c8b5f1b7041b17612211502140b1c368181a15
koudai0024 <iwai.0731v@gmail.com>
2020-12-09 23:37:08 +0900

    fix sitemap generator

--- sitemap.js
+++ sitemap.js
@@ -5,8 +5,8 @@ const BUILD_ID = fs.readFileSync(".next/BUILD_ID").toString();
 
 sitemap({
   baseUrl: "https://kimamaniweb.com",
-  pagesDirectory: __dirname + "/out/",
-  targetDirectory: "public/",
+  pagesDirectory: __dirname + "/.next/server/pages/",
+  targetDirectory: "out/",
   ignoredExtensions: ["js", "map", "ico", "xml", "svg"],
-  ignoredPaths: ["[fallback]", "image", "_next", "page", "google85f37d88fa2c41e5"],
+  ignoredPaths: ["[fallback]", "image", "_next", "page"],
 });
