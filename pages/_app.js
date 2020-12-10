3aa4e479e3305a0645cebb3fbfb1a734f186ac33
岩井航大 <iwaikoudai@mbp.local>
2020-12-10 14:46:20 +0900

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
 import '../styles/reset.css'
 import '../styles/globals.css'
 import 'highlight.js/styles/hybrid.css';
-function MyApp({ Component, pageProps }) {
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
