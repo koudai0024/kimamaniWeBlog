0192593270aa756935b61bd13ea744c9cea9ceff
岩井航大 <iwaikoudai@mbp.local>
2020-12-10 14:25:24 +0900

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '../styles/reset.css'
 import '../styles/globals.css'
 import 'highlight.js/styles/hybrid.css';
+import Router from 'next/router'
+import * as gtag from '../lib/gtag'
+
+Router.events.on('routeChangeComplete', url => gtag.pageview(url))
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
